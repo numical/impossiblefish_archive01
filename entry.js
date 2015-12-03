4a64e9101443828bb13b379f54c58c7f82dd28e5
Mike Evans <mike.evans@numical.com>
2015-12-03 22:35:25 +0000

    very basic fishtank onload sizing

--- entry.js
+++ entry.js
@@ -1,2 +1,16 @@
 require( './impossiblefish.css' )
 
+window.onload = function(){
+ 
+ function $( selector, container ) {
+  return ( container || document ).querySelector( selector );
+ }
+
+ var canvas = $('canvas');
+ var fishtank = $('#fishtank');
+ canvas.width=fishtank.clientWidth;
+ canvas.height=fishtank.clientHeight;
+
+
+};
+
