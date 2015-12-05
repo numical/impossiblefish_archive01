058855e0ba82268188433691aa2fe97d81fc44d9
Mike Evans <mike.evans@numical.com>
2015-12-05 16:11:56 +0000

    working when upscaling using flexbox

--- entry.js
+++ entry.js
@@ -2,17 +2,28 @@ require( './impossiblefish.css' )
 
 window.onload = function(){
  
- var canvas = $('canvas');
- var fishtank = $('#fishtank');
+ var canvas = $('canvas'), 
+     fishtank = $('#fishtank'),
+     cachedCanvasBorderWidth = -1;                       
 
  function $( selector, container ) {
   return ( container || document ).querySelector( selector );
  }
 
- function resize(){
-  canvas.width=fishtank.clientWidth - 20;
-  canvas.height=fishtank.clientHeight - 20;
+ function canvasBorderWidth() {
+  if ( cachedCanvasBorderWidth < 0 ) {
+   var style = canvas.currentStyle || window.getComputedStyle( canvas );
+   cachedCanvasBorderWidth = parseFloat( style.borderWidth.slice( 0, -2 ) ) ;                                 
+  }
+  return cachedCanvasBorderWidth;
  }
+
+ function resize(){
+  canvas.width=fishtank.clientWidth - canvasBorderWidth() * 4;
+  canvas.height=fishtank.clientHeight - canvasBorderWidth() * 2;
+  console.log( canvas.width + ", " + canvas.height );
+ }
+
 
  window.addEventListener( 'resize', resize, false );
  window.addEventListener('orientationchange', resize, false );
