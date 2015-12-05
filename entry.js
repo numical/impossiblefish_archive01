a5e17ea4ef51b40ff7c3966ba4f258042ba576d8
Mike Evans <mike.evans@numical.com>
2015-12-05 20:20:24 +0000

    introspect canvas borders

--- entry.js
+++ entry.js
@@ -4,26 +4,28 @@ window.onload = function(){
  
  var canvas = $('canvas'), 
      fishtank = $('#fishtank'),
-     cachedCanvasBorderWidth = -1;                       
+     canvasBorders = introspectBorders( canvas );             
 
  function $( selector, container ) {
   return ( container || document ).querySelector( selector );
  }
 
- function canvasBorderWidth() {
-  if ( cachedCanvasBorderWidth < 0 ) {
-   var style = canvas.currentStyle || window.getComputedStyle( canvas );
-   cachedCanvasBorderWidth = parseFloat( style.borderWidth.slice( 0, -2 ) ) ;                                 
-  }
-  return cachedCanvasBorderWidth;
+ function introspectBorders( element ) {
+  var style = window.getComputedStyle( element ), borders = {};
+  borders.top = parseFloat( style.borderTopWidth.slice( 0, -2 ) );
+  borders.bottom = parseFloat( style.borderBottomWidth.slice( 0, -2 ) );
+  borders.left = parseFloat( style.borderLeftWidth.slice( 0, -2 ) );
+  borders.right = parseFloat( style.borderRightWidth.slice( 0, -2 ) ) ;
+  borders.horizontal = borders.left + borders.right;
+  borders.vertical= borders.top + borders.bottom;
+  return borders;
  }
 
  function resize(){
-  canvas.width=fishtank.clientWidth - canvasBorderWidth() * 4;
-  canvas.height=fishtank.clientHeight - canvasBorderWidth() * 2;
+  canvas.width=fishtank.clientWidth - canvasBorders.horizontal * 2;
+  canvas.height=fishtank.clientHeight - canvasBorders.vertical;
   console.log( canvas.width + ", " + canvas.height );
  }
-
 
  window.addEventListener( 'resize', resize, false );
  window.addEventListener('orientationchange', resize, false );
