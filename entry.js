7a346c34f04fa7baf3c7167f47acbf8b4a678909
Mike Evans <mike.evans@numical.com>
2015-12-07 22:05:41 +0000

    tidied (questionably) entry.js

--- entry.js
+++ entry.js
@@ -2,44 +2,54 @@ require( './impossiblefish.css' )
 
 window.onload = function(){
  
- var canvas = $('canvas'), 
-     fishtank = $('#fishtank'),
-     canvasBorders = introspectBorders( canvas );             
-
  function $( selector, container ) {
   return ( container || document ).querySelector( selector );
  }
 
- function parsePixelProperty( property ) {
-  return parseFloat( property.slice( 0, -2 ) );
- }
+ function initDynamicResize( fishtank, canvas ) {
 
- // assumes borders defined as 'Npx'
- function introspectBorders( element ) {
-  var style = window.getComputedStyle( element ), borders = {};
-  borders.top = parsePixelProperty( style.borderTopWidth );
-  borders.bottom = parsePixelProperty( style.borderBottomWidth );
-  borders.left = parsePixelProperty( style.borderLeftWidth );
-  borders.right = parsePixelProperty( style.borderRightWidth ) ;
-  borders.horizontal = borders.left + borders.right;
-  borders.vertical= borders.top + borders.bottom;
-  return borders;
- }
-
- function resize(){
-  if ( canvas.width >= fishtank.clientWidth ) {
-   canvas.width = 0;
-   canvas.height = 0;
+  function parsePixelProperty( property ) {
+   return parseFloat( property.slice( 0, -2 ) );
   }
-  canvas.width=fishtank.clientWidth - canvasBorders.horizontal * 2;
-  canvas.height=fishtank.clientHeight - canvasBorders.vertical;
-  // console.log( canvas.width + ", " + canvas.height );
+
+  // assumes borders defined as 'Npx'
+  function introspectBorders( element ) {
+   var style = window.getComputedStyle( element ), borders = {};
+   borders.top = parsePixelProperty( style.borderTopWidth );
+   borders.bottom = parsePixelProperty( style.borderBottomWidth );
+   borders.left = parsePixelProperty( style.borderLeftWidth );
+   borders.right = parsePixelProperty( style.borderRightWidth ) ;
+   borders.horizontal = borders.left + borders.right;
+   borders.vertical= borders.top + borders.bottom;
+   return borders;
+  }
+
+  function resize(){
+   if ( canvas.width >= fishtank.clientWidth ) {
+    canvas.width = 0;
+    canvas.height = 0;
+   }
+   canvas.width=fishtank.clientWidth - canvasBorders.horizontal * 2;
+   canvas.height=fishtank.clientHeight - canvasBorders.vertical;
+   // console.log( canvas.width + ", " + canvas.height );
+  }
+
+  var canvasBorders = introspectBorders( canvas );             
+  window.addEventListener( 'resize', resize, false );
+  window.addEventListener('orientationchange', resize, false );
+  resize();
  }
 
- window.addEventListener( 'resize', resize, false );
- window.addEventListener('orientationchange', resize, false );
+ function initMenu( canvas ){
+  canvas.addEventListener( 'click', function(){
+   alert("clicked");
+  });
+ }
 
- resize();
+ var canvas = $('canvas'), 
+     fishtank = $('#fishtank');
+ initDynamicResize( fishtank, canvas );
+ initMenu( canvas );
 
 };
 
